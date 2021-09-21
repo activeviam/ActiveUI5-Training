0725a029b308976bd59e8406726d50e584fee83a
Clement Caylux <cca@activeviam.com>
2021-09-21 18:20:56 +0200

    setting up the servers and updating gitignore

--- env.development.js
+++ env.development.js
@@ -1,18 +1,12 @@
 window.env = {
   contentServerVersion: "5.10.x",
-  contentServerUrl: "https://activepivot-ranch.activeviam.com:5100",
+  contentServerUrl: "http://localhost:9090",
   // WARNING: Changing the keys of activePivotServers will break previously saved widgets and dashboards.
   // If you must do it, then you also need to update each one's serverKey attribute on your content server.
   activePivotServers: {
-    "Ranch 5.10": {
-      url: "https://activepivot-ranch.activeviam.com:5100",
-      version: "5.10.0",
-    },
-    // You can connect to as many servers as needed.
-    // In practice most projects only need one.
-    "Ranch 5.9": {
-      url: "https://activepivot-ranch.activeviam.com:5900",
-      version: "5.9.4",
-    },
+    "sandbox-5.8.7": {
+      url: "http://localhost:9090",
+      version: "5.10.x",
+    }
   },
 };
