24309d3ec2eb2ed54056dc486b229a55243f24d6
Clément Caylux <cca@activeviam.com>
2021-09-22 14:28:35 +0200

    Initial (#2)
    * initial commit
    
    * setting up the servers and updating gitignore
    
    * updating .gitignor
    
    * updating the README and adding initial-content.json
    
    * updating readme
    
    * updating readme
    
    * adding comments
    
    * adding comments
    
    * typo

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,23 @@
 import {
   ExtensionModule,
-  pluginWidgetPivotTable,
+  pluginWidgetPivotTable, pluginWidgetPlotlyLineChart,
 } from "@activeviam/activeui-sdk";
 import { withSandboxClients } from "@activeviam/sandbox-clients";
 import _merge from "lodash/merge";
 import { plugins } from "./plugins";
 
+/** defines the first page of the application where
+- The withSandboxclients allows user login
+- the plugins are registered
+- One can register the name of the application
+- the initial state of every new dashboard is defined
+ */
 const extension: ExtensionModule = {
   activate: async (configuration) => {
     _merge(configuration.pluginRegistry, plugins);
     configuration.applicationName = "ActiveUI";
     configuration.initialDashboardPageState = {
-      content: { "0": pluginWidgetPivotTable.initialState },
+      content: { "0": pluginWidgetPlotlyLineChart.initialState },
       layout: {
         children: [
           {
