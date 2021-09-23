d6c7a52710c21944837362acdc01b2756c0a2f39
Clement Caylux <cca@activeviam.com>
2021-09-23 11:34:31 +0200

    adding to do

--- src/custom/plugin/CurrencyExchange/FxWidget.tsx
+++ src/custom/plugin/CurrencyExchange/FxWidget.tsx
@@ -3,7 +3,7 @@ import { FXComponent } from "./FXComponent";
 
 const widgetKey = "fx-rates"
 
-//TODO Ex2: Register the FxComponent to the FxWidget
+//TODO Ex2-1: Register the FxComponent to the FxWidget
 export const pluginFxRateWidget: WidgetPlugin = {
     Component: "Insert component here",
     initialState: {
