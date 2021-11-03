6d64938a5d718da22f83fd852cac809694807b08
Clement Caylux <cca@activeviam.com>
2021-11-03 15:49:47 +0100

    displaying a simple table

--- src/custom/plugin/CurrencyExchange/FXComponent.tsx
+++ src/custom/plugin/CurrencyExchange/FXComponent.tsx
@@ -3,9 +3,7 @@ import {Col, Row, Select, Table} from "antd";
 import axios from "axios"
 import React, {FC, useEffect, useState} from "react"
 
-const apiURL = "http://api.frankfurter.app/"
-const possibleBaseCurrencies=["USD","EUR"]
-const possibleDates=["latest", "2021-09-22"]
+const apiURL = "http://api.frankfurter.app/latest?from=AUD&to=CNY,BRL,ISK"
 const columns = [
     {
         title: 'Currency',
@@ -21,14 +19,11 @@ const columns = [
 
 export const FXComponent: FC<WidgetPluginProps> = () => {
 
-    const [base, setBase] = useState("USD");
-    const [date, setDate] = useState("latest");
     const [fxRates, setFxRates] = useState([]);
 
     // makes the API call to retrieve fxRates, base and date
     useEffect(() => {
-        axios.get(`${apiURL}${date}?from=${base}&to=GBP,EUR,AUD`).then(result => {
-            console.log("result.data")
+        axios.get(apiURL).then(result => {
 
             let ratesTableData: any[] = Object.keys(result.data.rates).map(currency => {
                 let data = {
@@ -41,9 +36,6 @@ export const FXComponent: FC<WidgetPluginProps> = () => {
             });
 
             console.log(ratesTableData)
-
-            setBase(result.data.base)
-            setDate(result.data.date)
             setFxRates(ratesTableData)
         }
         ).catch(()=>{
@@ -51,48 +43,17 @@ export const FXComponent: FC<WidgetPluginProps> = () => {
         })
     }, []);
 
-    const baseCurrencyOptions = possibleBaseCurrencies.map(currency => {
-        console.log(currency);
-        return (
-            <Select.Option value={currency}>{currency}</Select.Option>
-            // {label: currency, value: currency}
-        );
-    });
 
-    const handleChange = (event: any) => {
-        console.log(event.currentTarget.value);
-        setBase(event.currentTarget.value)
-    }
 
     return (
-        <div style={{height:"100%", overflow: "auto"}}>
-            {/*<Select*/}
-            {/*    defaultValue={date}*/}
-            {/*    style={{ width: 120 }}*/}
-            {/*    placeholder="select a date"*/}
-            {/*>*/}
-            {/*    {*/}
-            {/*        possibleDates.map(date => (*/}
-            {/*                <Option value={date}>{date}</Option>*/}
-            {/*            )*/}
-            {/*        )*/}
-            {/*    }*/}
-            {/*</Select>*/}
-            {/*<Table*/}
-            {/*    columns={columns}*/}
-            {/*    dataSource={fxRates}*/}
-            {/*    size="small"*/}
-            {/*    bordered*/}
-            {/*    pagination={false}*/}
-            {/*/>*/}
-                <Select
-                    style={{ padding: "4px 4px", width: 120 }}
-                    // options={baseCurrencyOptions}
-                >
-                    <Select.Option value={1}>test1</Select.Option>
-                    <Select.Option value={2}>test2</Select.Option>
-                </Select>
-
+        <div style={{height:"100%", overflow: "auto" , padding: 5}}>
+            <Table
+                columns={columns}
+                dataSource={fxRates}
+                size="small"
+                bordered
+                pagination={false}
+            />
         </div>
     )
 }
