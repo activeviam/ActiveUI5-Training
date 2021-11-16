5e8bc401f2809e325240da3e900b2591790a02c7
Clement Caylux <cca@activeviam.com>
2021-11-16 16:54:27 +0100

    adding to do to FxComponent.types.ts

--- src/custom/plugin/CurrencyExchange/FxComponent.types.ts
+++ src/custom/plugin/CurrencyExchange/FxComponent.types.ts
@@ -1,7 +1,6 @@
 import {AWidgetState, MdxString} from "@activeviam/activeui-sdk";
 
-// TODO: Ex4 - Add the query and the baseCurrency to the widgetState
+// TODO: Ex4 - Add the query and the baseCurrency to the widgetState. The query should be typed as an mdx string
 export interface FxComponentWidgetState extends AWidgetState {
-}
 
 }
