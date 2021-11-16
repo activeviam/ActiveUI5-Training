244a648dbe4b062141aed0821f4205766148e2ab
Clement Caylux <cca@activeviam.com>
2021-11-16 11:04:53 +0100

    adding to do

--- src/custom/plugin/CurrencyExchange/FXComponent.tsx
+++ src/custom/plugin/CurrencyExchange/FXComponent.tsx
@@ -20,8 +20,15 @@ const columns = [
 export const FXComponent: FC<WidgetPluginProps> = () => {
 
     const [fxRates, setFxRates] = useState([]);
+    //TODO: Ex3-1 Add baseCurrency as state and set it to dollars as initial state. We will use this in future exercises
 
     // makes the API call to retrieve fxRates, base and date
+
+    // TODO:  Ex 3-2
+    //  1/ use a pivot table to build the query that will allow you to retrieve the currencies from the cube
+    //  2/ import the the useQueryResult hook and use it with your query to retrieve the currencies. Don't forget to
+    //  3/ use the returned values to: display  a spin if the query has not returned, print the stacktrace of the error if there is one, and finally pass the returned currencies to the api call  to get the rates.
+
     useEffect(() => {
         axios.get(apiURL).then(result => {
 
