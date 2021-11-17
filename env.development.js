f1c5e6c21050ea587486c7333123456628224993
Clement Caylux <cca@activeviam.com>
2021-11-17 22:37:46 +0100

    changing back to use localhost

--- env.development.js
+++ env.development.js
@@ -1,12 +1,12 @@
 window.env = {
-  contentServerVersion: "5.10.x",
-  contentServerUrl: "https://activepivot-ranch.activeviam.com:5100",
+  contentServerVersion: "5.10.4",
+  contentServerUrl: "http://localhost:9090",
   // WARNING: Changing the keys of activePivotServers will break previously saved widgets and dashboards.
   // If you must do it, then you also need to update each one's serverKey attribute on your content server.
   activePivotServers: {
-    "Ranch-5.10": {
-      url: "https://activepivot-ranch.activeviam.com:5100",
-      version: "5.10.x",
+    "sandbox-5.8.18": {
+      url: "http://localhost:9090",
+      version: "5.10.4",
     }
   },
 };
